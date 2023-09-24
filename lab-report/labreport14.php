e6c983b61813f177ca0a2f1bc073a14e6feb7bb3
Roshan Limbu <topazkayle@gmail.com>
2023-09-24 21:42:24 +0545

    lab report 14 done

---
+++ lab-report/labreport14.php
@@ -0,0 +1,23 @@
+<?php
+echo "Hello PHP <br>";
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create a connection
+$conn = mysqli_connect($servername, $username, $password);
+
+$sql = "CREATE DATABASE namna_college";
+query($sql);
+
+
+if (!$conn){
+    die("Connecton error ". mysqli_connect_error());
+}
+else{
+    echo "Connection was successful";
+}
+
+?>
+
